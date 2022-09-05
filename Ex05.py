7eb66d2ba85f2f1c54ff3f14491971cf9fd3cd63
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2022-09-05 08:43:19 +0800

    feat: refactoring ex05 with newline

--- Ex05.py
+++ Ex05.py
@@ -2,11 +2,11 @@
 print("Hello My program")
 
 #2. Ask the user for the city that grew up in
-city = input("Where did you grow up? ")
+city = input("Where did you grow up?\n ")
 
 
 #3. Ask the user for the name of a pet
-pet_name = input("What is your pet's name? ")
+pet_name = input("What is your pet's name?\n ")
 
 #4. Combine the name of their city and pet and show them their band name
 
