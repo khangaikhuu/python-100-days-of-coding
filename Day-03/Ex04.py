e596741be54cb0c7f5656a8e09beb979df069aa8
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2022-09-07 07:53:18 +0800

    feat: fixing some conditionals in the bmi calculation

--- Day-03/Ex04.py
+++ Day-03/Ex04.py
@@ -6,11 +6,11 @@ bmi = int(weight / (height**2) )
 
 if bmi < 18.5:
  print('You are underweight')
-elif bmi >= 18.5 and bmi < 25:
+elif bmi < 25:
  print('You are in normal weight')
-elif bmi >=25 and bmi < 30:
+elif bmi < 30:
  print('You are overweight')
-elif bmi >= 30 and bmi < 35:
+elif bmi < 35:
  print('You are obese')
 else:
  print('You are clinically obese')
