aa7063964058f5f6ea2cbeba1b86a55041c34c33
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2022-09-07 07:24:12 +0800

    feat: adding even odd number solution

---
+++ Day-03/Ex02.py
@@ -0,0 +1,9 @@
+number = int(input("which number do you want to check?\n"))
+
+even_odd = number % 2
+
+if even_odd == 0:
+ print('This is an even number.')
+else:
+ print('This is an odd number.')
+
