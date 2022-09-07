a21928b6b0b2ce986e8ed721906c47a01a1518ab
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2022-09-07 09:23:20 +0800

    feat: adding some changes to the ex 9 love score challenge

--- Day-03/Ex09.py
+++ Day-03/Ex09.py
@@ -16,16 +16,23 @@ print(f"E occurs {e}")
 true = t + r + u + e
 
 l = combined_name.count('l')
-print(f"L occurs {counter1}")
+print(f"L occurs {l}")
 o = combined_name.count('o')
-print(f"O occurs {counter1}")
+print(f"O occurs {o}")
 v = combined_name.count('v')
-print(f"V occurs {counter1}")
+print(f"V occurs {v}")
 e = combined_name.count('e')
-print(f"E occurs {counter1}")
+print(f"E occurs {e}")
 
 love = l + o + v + e 
 
 result = str(true) + str(love) 
 print(result)
+love_score = int(result)
 
+if (love_score < 10) or (love_score > 90):
+ print(f"Your love score is {love_score}, you go together like coke and mentos.")
+elif (love_score >=40) and (love_score <=50):
+ print(f"Your love score is {love_score}, you are alright together.")
+else:
+ print(f"Your score is {love_score}")
