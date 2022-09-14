0f015e31bd13b19d4a1c1d711fe695b1e093b1e7
khangaikhuu uvgunkhuu <khangaikhuu@gmail.com>
2022-09-14 21:34:43 +0800

    feat: updating ex01.py with early return;

--- Day-10/Ex01.py
+++ Day-10/Ex01.py
@@ -1,4 +1,6 @@
 def format_name(first_name, last_name):
+ if first_name == "" or last_name == "":
+  return "You did not provide valid inputs"
  f_name = first_name.title()
  l_name = last_name.title()
  return f"{f_name} {l_name}"
