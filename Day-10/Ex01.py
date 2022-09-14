201b54bde910e641e6757f62e25b6d7f4b5507f1
khangaikhuu uvgunkhuu <khangaikhuu@gmail.com>
2022-09-14 21:28:50 +0800

    feat: adding Ex01.py solution

---
+++ Day-10/Ex01.py
@@ -0,0 +1,9 @@
+def format_name(first_name, last_name):
+ f_name = first_name.title()
+ l_name = last_name.title()
+ return f"{f_name} {l_name}"
+
+output = format_name("angela","ANGELA")
+print(output)
+
+
